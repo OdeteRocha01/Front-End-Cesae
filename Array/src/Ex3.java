e7776609c1c1351131e150e8b8803f68329609cf
Odete Rocha <odeterocha1971@outlook.pt>
2025-10-07 12:39:07 +0100

    w

---
+++ Array/src/Ex3.java
@@ -0,0 +1,10 @@
+public class Ex3 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner (system.in);
+
+        double []maior comissao= new maior comissao[];
+        maioe comissao = new [9]
+    }
+}
